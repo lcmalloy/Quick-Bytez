db2a8b6e25e357d039f508d56d5b2c8732afc748
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-08 16:10:16 -0800

    setup grid layout for header, nav and Hero section added text style, font size, and color based on wireframe. added responsive css for Hero section and Header. Hides a tags and image when dimension is smaller than 800px. grid layout for Hero becomes 1 row when dimension smaller than 800px;

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import "./Hero.css"
+import './Hero.css'
+import heroImage from '../assets/restauranfood.jpg'
 
 const Hero = () => {
   return (
-    <div className="hero_main">Hero</div>
+    <div className="hero_main hero_grid">
+      <div className="flex-content">
+        <div className="hero_content">
+          <h1 className="primary2 heading">Little Lemon</h1>
+          <h2 className="secondary2 subheading">Chicago</h2>
+          <p className="secondary2">Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. The restaurant features a locally-sourced menu with daily specials.</p>
+          <button className="btn1">Reserve a table</button>
+        </div>
+      </div>
+      <div className="hero_img_container">
+        <img src={heroImage} className="hero_image" alt="restaurant"/>
+      </div>
+    </div>
   )
 }
 
