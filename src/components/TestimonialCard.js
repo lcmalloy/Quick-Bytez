f176c74dc277006318c8db219e3369f7b9446d4f
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-10 15:12:10 -0800

    testimonial feature: updated styling of testimonial card moved css class to global css if necessary added styling to section description.

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -4,10 +4,12 @@ import '../styles/TestimonialCard.css'
 const TestimonialCard = (props) => {
   return (
     <div className="card-testimonial">
-      <h3>{props.testimonial.rating}</h3>
-      <img src='' alt='profile'/>
-      <h3>{props.testimonial.name}</h3>
-      <p>{props.testimonial.review}</p>
+      <h3 className="ml-20">Rating: {props.testimonial.rating}</h3>
+      <div className='profile ml-5 inline-b'>
+        <img className="img-sm" src={props.testimonial.profile} alt=""/>
+      </div>
+      <h3 className="name inline-f">{props.testimonial.name}</h3>
+      <p className="testimonial-description">{props.testimonial.review}</p>
     </div>
   )
 }
--- src/container/Testimonials/Testimonials.js
+++ src/container/Testimonials/Testimonials.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import "../../styles/Testimonials.css"
 import TestimonialCard from '../../components/TestimonialCard'
+import profile1 from '../../assets/profile1.jpg'
+import profile2 from '../../assets/profile2.jpg'
+import profile3 from '../../assets/profile3.jpg'
 
 const testimonials = [
   {
     name: 'Ken Mars',
     rating: 4,
-    review: 'Culpa adipisicing ullamco amet tempor labore enim.'
+    review: 'Culpa adipisicing ullamco amet tempor labore enim.',
+    profile: profile1,
   },
   {
     name: 'Kyle Marks',
     rating: 5,
-    review: 'Qui irure voluptate deserunt ad laboris.'
+    review: 'Qui irure voluptate deserunt ad laboris.',
+    profile: profile3,
   },
   {
     name: 'Jessica Chu',
     rating: 4,
-    review: 'Aliquip deserunt pariatur consequat minim consectetur adipisicing sint excepteur ad irure sunt nulla officia laboris.'
+    review: 'Aliquip deserunt pariatur consequat minim consectetur adipisicing sint excepteur ad irure sunt nulla officia laboris.',
+    profile: profile2,
   }
 ]
 
@@ -27,7 +33,7 @@ const Testimonials = () => {
   )
   return (
     <div className="testimonials_main">
-      Testimonials
+      <h2 className='flex-center mt-50'>Testimonials</h2>
       <div className="testimonial_body">
         {testimonial}
       </div>
