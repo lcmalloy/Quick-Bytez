36af73aa54cfaa2a391bdf0b561548514e17f6a7
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-10 12:49:13 -0800

    card-testimonial feature added style to card component adjusted margin for better seperation

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/TestimonialCard.css'
 
 const TestimonialCard = (props) => {
   return (
--- src/container/Testimonials/Testimonials.js
+++ src/container/Testimonials/Testimonials.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import "../../styles/Testimonials.css"
+import TestimonialCard from '../../components/TestimonialCard'
 
 const testimonials = [
   {
     name: 'Ken Mars',
     rating: 4,
-    
+    review: 'Culpa adipisicing ullamco amet tempor labore enim.'
+  },
+  {
+    name: 'Kyle Marks',
+    rating: 5,
+    review: 'Qui irure voluptate deserunt ad laboris.'
+  },
+  {
+    name: 'Jessica Chu',
+    rating: 4,
+    review: 'Aliquip deserunt pariatur consequat minim consectetur adipisicing sint excepteur ad irure sunt nulla officia laboris.'
   }
 ]
 
 const Testimonials = () => {
+
+  let testimonial = testimonials.map((testimonial) =>
+    <TestimonialCard testimonial={testimonial}/>
+  )
   return (
-    <div className="testimonials_main">Testimonials</div>
+    <div className="testimonials_main">
+      Testimonials
+      <div className="testimonial_body">
+        {testimonial}
+      </div>
+    </div>
   )
 }
 
