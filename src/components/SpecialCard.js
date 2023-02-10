bcaf17194738464475c5d51d88af3651060deb77
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-09 16:41:30 -0800

    configured new component: specials meal cards added heading, image, price, description and action link to feature configured css style for card *temp will need updating for mobile version. updated rules for css grid. will need to update all sections on home page to fit components correctly." "

---
+++ src/components/SpecialCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/SpecialCard.css'
+
+import { Link } from 'react-router-dom'
+
+const SpecialCard = (props) => {
+  console.log(props)
+  return (
+    <div className="card-specials">
+      <img src={props.meal.img} alt={props.meal.alt}/>
+      <div className="card-info-head">
+        <h3 className="title">{props.meal.name}</h3>
+        <h3 className="price secondary1">{props.meal.price}</h3>
+      </div>
+      <div className="card-info-body">
+        <p className="card-text">{props.meal.description}</p>
+      </div>
+      <div className="card-action">
+        <Link to='/OrderOnline' className="nav-item">Order a Delivery</Link>
+      </div>
+    </div>
+  )
+}
+
+export default SpecialCard
--- src/container/Highlights/Highlights.js
+++ src/container/Highlights/Highlights.js
@@ -1,9 +1,51 @@
 import React from 'react'
 import "../../styles/Highlights.css"
+import "../../styles/Button.css"
+import SpecialCard from '../../components/SpecialCard'
+import SaladImg from '../../assets/greek salad.jpg'
+import BruchettaImg from '../../assets/bruchetta.svg'
+import LemonDessertImg from '../../assets/lemon dessert.jpg'
+
+
+const specials = [
+  {
+    name: 'Greek Salad',
+    price: '$12.99',
+    description: 'The famous greek salad of crispy lettuce, peppers, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+    img: SaladImg,
+    alt: 'salad',
+  },
+  {
+    name: 'Bruchetta',
+    price: '$5.99',
+    description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic, and seasoned with sald and olive oil.',
+    img: BruchettaImg,
+    alt: 'Bruchetta',
+  },
+  {
+    name: 'Lemon Dessert',
+    price: '$5.00',
+    description: 'This comes straight from grandma`s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+    img: LemonDessertImg,
+    alt: 'lemon dessert',
+  }
+]
 
 const Highlights = () => {
+
+  const special = specials.map((meal) =>
+    <SpecialCard meal={meal} />
+  )
   return (
-    <div className="hightlights_main">Highlights</div>
+    <div className="hightlights_main">
+      <div className="highlight-heading">
+        <h2>This weeks specials</h2>
+        <button type="button" className="btn1-square">Online Menu</button>
+      </div>
+      <div className="container-special">
+        {special}
+      </div>
+    </div>
   )
 }
 
