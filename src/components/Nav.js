480462bbb3ef4e53fdc66b0118dc6bdeb74f5cb1
Lawrence Malloy <lcmalloy@gmail.com>
2023-02-10 17:47:51 -0800

    styled footer feature. added 4 col grid for each section added css style for links updated naming convention for Nav class

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,12 +7,12 @@ const Nav = () => {
   return (
     <>
       <nav className="navbar_main">
-        <Link to='/' className="nav-item">HOME</Link>
-        <Link to='/Menu' className="nav-item">MENU</Link>
-        <Link to='/About' className="nav-item">ABOUT</Link>
-        <Link to='/Reservation' className="nav-item">RESERVATION</Link>
-        <Link to='/OrderOnline' className="nav-item">ORDER ONLINE</Link>
-        <Link to='/Login' className="nav-item">LOGIN</Link>
+        <Link to='/' className="nav-link">HOME</Link>
+        <Link to='/Menu' className="nav-link">MENU</Link>
+        <Link to='/About' className="nav-link">ABOUT</Link>
+        <Link to='/Reservation' className="nav-link">RESERVATION</Link>
+        <Link to='/OrderOnline' className="nav-link">ORDER ONLINE</Link>
+        <Link to='/Login' className="nav-link">LOGIN</Link>
       </nav>
     </>
   )
