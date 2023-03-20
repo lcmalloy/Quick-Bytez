5b468c4594061caa2e32332af7dc47e79e0531e7
Lawrence Malloy <lcmalloy@gmail.com>
2023-03-20 16:37:15 -0700

    updated mobile view

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,26 +11,28 @@ const Footer = () => {
         <div className="container__footer-img">
           <img src={Restaurant} alt="" />
         </div>
-        <div className="container__footer-nav">
-          <h2>Doormat Navigation</h2>
-          <Link to='/Quick-Bytez/' className="foot-link">HOME</Link>
-          <Link to='/Quick-Bytez/Menu' className="foot-link">MENU</Link>
-          <Link to='/Quick-Bytez/About' className="foot-link">ABOUT</Link>
-          <Link to='/Quick-Bytez/Reservation' className="foot-link">RESERVATION</Link>
-          <Link to='/Quick-Bytez/OrderOnline' className="foot-link">ORDER ONLINE</Link>
-          <Link to='/Quick-Bytez/Login' className="foot-link">LOGIN</Link>
-        </div>
-        <div className="container__footer-contact">
-          <h2>Contacts</h2>
-          <h3>Address</h3>
-          <h3>Phone Number</h3>
-          <h3>Email</h3>
-        </div>
-        <div className="container__footer-socials">
-          <h2>Social Media Links</h2>
-          <h3>Facebook</h3>
-          <h3>Instagram</h3>
-          <h3>Twitter</h3>
+        <div className="container__footer-content">
+          <div className="container__footer-nav">
+            <h2>Doormat Navigation</h2>
+            <Link to='/Quick-Bytez/' className="foot-link">HOME</Link>
+            <Link to='/Quick-Bytez/Menu' className="foot-link">MENU</Link>
+            <Link to='/Quick-Bytez/About' className="foot-link">ABOUT</Link>
+            <Link to='/Quick-Bytez/Reservation' className="foot-link">RESERVATION</Link>
+            <Link to='/Quick-Bytez/OrderOnline' className="foot-link">ORDER ONLINE</Link>
+            <Link to='/Quick-Bytez/Login' className="foot-link">LOGIN</Link>
+          </div>
+          <div className="container__footer-contact">
+            <h2>Contacts</h2>
+            <h3>Address</h3>
+            <h3>Phone Number</h3>
+            <h3>Email</h3>
+          </div>
+          <div className="container__footer-socials">
+            <h2>Social Media Links</h2>
+            <h3>Facebook</h3>
+            <h3>Instagram</h3>
+            <h3>Twitter</h3>
+          </div>
         </div>
       </div>
     </section>
